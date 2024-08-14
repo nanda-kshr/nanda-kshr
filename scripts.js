44e88c201981bdddccb9c60d12c07c05721094a1
Nandakishore P <95837974+nanda-kshr@users.noreply.github.com>
2024-08-14 09:21:03 +0530

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    // Smooth scrolling
+    $('a.nav-link').on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function(){
+                window.location.hash = hash;
+            });
+        }
+    });
+});
